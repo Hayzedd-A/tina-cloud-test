6345af58a90575dbe30c877bdd64ffab09c6aecd
David Olubaji <lildeyveed@gmail.com>
2020-11-09 19:12:36 +0100

    change toppings style

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "cdbe324f6486f0b34e6d15e15ce84de2"
+    "revision": "e4de53b3ebe8a1dd45665b1bb27eafb3"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
