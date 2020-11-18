d19477939e6f8a0e4541dd2a6e2b004f604c975a
David Olubaji <lildeyveed@gmail.com>
2020-11-18 12:34:43 +0100

    adjust empty state heights

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "7935bbdff81132369edf3659810bc628"
+    "revision": "ce07d212b6cbb61590ee33312cf161cb"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
