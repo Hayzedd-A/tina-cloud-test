d6c75a677478266e655e4300bc75375ed7e43dcd
JohnDamilola <johndamilola03@gmail.com>
2021-04-26 20:28:29 +0100

    Change to dev server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://zupa-prod-api.dev.intelia.io/",
+  baseURL: "https://zupa-api.dev.intelia.io/",
   headers: {}
 });
 
