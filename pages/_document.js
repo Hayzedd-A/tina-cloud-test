4950924cec80fa7591fb859b734db362578fc296
olamide jubril <olamidejubril68@gmail.com>
2022-11-16 10:58:29 +0100

    Added google tag manager script

--- pages/_document.js
+++ pages/_document.js
@@ -52,6 +52,33 @@ export default class MyDocument extends Document {
 
           <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDmIEtz7r7jGYPVGKfMAp-6SiMTfGMrMPI&libraries=places"></script>
           <script src="https://js.paystack.co/v1/inline.js"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+              j.async = true;
+              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, "script", "dataLayer", "GTM-MR6QMKX");
+              `,
+            }}
+          ></script>
+
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MR6QMKX"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>`,
+            }}
+          ></noscript>
         </Head>
         <body>
           <Main />
