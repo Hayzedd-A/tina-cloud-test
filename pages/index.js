7670aa126a9daa361630d0c58b2e779570191830
unknown <junaid.qayyum@mmmtechltd.com>
2024-09-28 15:53:56 +0500

    Showing popup message for order processing

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,15 @@ import SplashScreen from "../components/SplashScreen";
 import { ProductsConsumer } from "../providers/ProductsProvider";
 import { slugify } from "../utils/functions";
 
+import Modal from "../components/Modal";
+import { Logo } from "../public/static/vectors";
+
 class Home extends Component {
   state = {
     selectedItem: {},
     showDetails: false,
     showSplash: true,
+    showModal: true
   };
 
   selectItem = ({ name, id }) => {
@@ -33,7 +37,11 @@ class Home extends Component {
     clearTimeout(this.splashtimeout);
   }
 
-  // <SplashScreen />
+  closeModal = () => {
+    this.setState({
+      showModal: false,
+    });
+  }
 
   render() {
     const { isLoadingProducts } = this.props;
@@ -43,7 +51,30 @@ class Home extends Component {
         {isLoadingProducts ? (
           <SplashScreen />
         ) : (
-          <Shop selectItem={this.selectItem} />
+          <>
+            {
+              this.state.showModal && <Modal closeModal={this.closeModal}>
+                <div className="add-cart-success">
+                  <div className="icon">
+                    <Logo />
+                  </div>
+
+                  <div className="message">
+                    Dear customer, orders placed today will be processed on Monday, September 30 as we are relocating today.
+                    <br />
+                    Our new address is 19B Fola Osibo, Lekki.
+                  </div>
+
+                  <div className="actions">
+                    <button className="continue" onClick={this.closeModal}>
+                      Ok
+                    </button>
+                  </div>
+                </div>
+              </Modal>
+            }
+            <Shop selectItem={this.selectItem} />
+          </>
         )}
       </Main>
     );
