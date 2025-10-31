890b595fa93a7a7ac2d0ebac69784c470ba92328
Hayzedd-A <adebayoazeez37@yahoo.com>
2025-10-31 12:55:42 +0100

    update free delivery threshold from 10k to 25k

--- constants/index.js
+++ constants/index.js
@@ -14,7 +14,7 @@ export const CHOWDECK_API_URL = `https://api.chowdeck.com/merchant/${merchentRef
 // export const ANALYTICS_API_BASE_URL = 'http://localhost:3000/'
 export const ANALYTICS_API_BASE_URL = API_BASE_URL;
 export const paystack_env = "prod";
-export const FREE_DELIVERY_TRESHOLD = 10000
+export const FREE_DELIVERY_TRESHOLD = 25000
 
 
 export const KITCHEN_LOCATION = {
