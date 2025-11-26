c1ce9f69fd25f3ef3f313bbcbcb4eee886eef938
Hayzedd-A <adebayoazeez37@yahoo.com>
2025-11-26 12:23:13 +0100

    resolve api base url

--- constants/index.js
+++ constants/index.js
@@ -7,9 +7,9 @@ export const merchentRef = "ref_8d6cd1583f4ee33c29786a208d";
 
 export const CHOWDECK_KEY =
   "sk_test_7869ea89f1b52e7b48b0606afeaa1f9e74755f1fb83c5b8106";
-// export const API_BASE_URL = "https://api.zupa.ng/";
+export const API_BASE_URL = "https://api.zupa.ng/";
 // export const API_BASE_URL = "https://dev.api.zupa.ng/";
-export const API_BASE_URL = 'http://localhost:3000/';
+// export const API_BASE_URL = 'http://localhost:3000/';
 export const CHOWDECK_API_URL = `https://api.chowdeck.com/merchant/${merchentRef}`;
 // export const ANALYTICS_API_BASE_URL = 'http://localhost:3000/'
 export const ANALYTICS_API_BASE_URL = API_BASE_URL;
